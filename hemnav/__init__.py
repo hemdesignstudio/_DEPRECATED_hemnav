71db217895e7bb0b6b81a80d9cc58c8541857790
Albin Winkelmann <albin-w@hotmail.com>
2019-07-23 11:33:09 +0300

    Adds nav api files

---
+++ hemnav/__init__.py
@@ -0,0 +1,7 @@
+"""Nav Module
+
+This module handle all interactions with Microsoft Navision
+"""
+from .base import NAVBase
+from .inventory import Inventory
+from .sales_order import SalesOrder
---
+++ hemnav/settings.py
@@ -0,0 +1,38 @@
+import yaml, os
+from dotenv import load_dotenv
+from enum import Enum
+
+
+class RegionEnum(Enum):
+    EU = "eu"
+    US = "us"
+
+
+class Settings:
+    """Settings module for NAV Base wrapper
+    """
+
+    def __init__(self, region):
+        """
+        Args:
+            region (RegionEnum): NAV region config
+        Raises:
+            AttributeError: In case of wrong region
+        """
+        yaml_doc = open("config.yml", "r")
+        config = yaml.load(yaml_doc, Loader=yaml.FullLoader)
+        yaml_doc.close()
+
+        load_dotenv(".env")
+        environment = os.environ["ENV"].lower()
+
+        self.nav_username = os.environ["NAV_USERNAME"]
+        self.nav_password = os.environ["NAV_PASSWORD"]
+        self.base_url = config[environment]["base_url"]
+        self.store_name = config[environment][region.value]["store_name"].replace(
+            " ", "%20"
+        )
+
+
+if __name__ == "__main__":
+    Settings()
